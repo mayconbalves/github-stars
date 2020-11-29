d09c25e7408cd1e12da119c0b758596201b6fec5
mayconbalves <mayconmustaine@hotmail.com>
2020-11-29 10:56:11 -0300

    feat(*): create simplify reset styled

--- src/index.js
+++ src/index.js
@@ -3,9 +3,22 @@ import ReactDOM from 'react-dom'
 import reportWebVitals from './reportWebVitals'
 import Root from './Root'
 import store from './store/store'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    list-style: none;
+    font-family: Arial, sans-serif;
+    text-decoration: none;
+  }
+`
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <Root store={store} />
   </React.StrictMode>,
   document.getElementById('root')
