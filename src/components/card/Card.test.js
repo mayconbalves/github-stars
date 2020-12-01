cb07103e0cefcd9e32ab20f20d8f61c92aa1e0dc
mayconbalves <mayconmustaine@hotmail.com>
2020-12-01 10:24:40 -0300

    test(card): create simplify switchs of tests

---
+++ src/components/card/Card.test.js
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react'
+import Card from './'
+
+describe('<Card />', () => {
+  let props = {
+    cloneUrl: 'https://github.com/mayconbalves/app-go-barber.git',
+    license: 'MIT license',
+    stars: 1,
+    title: 'app go barber'
+  }
+
+  it('should render first title', () => {
+    const { container } = render(<Card {...props} />)
+    expect(screen.getByText(/Clone repo:/i)).toBeInTheDocument()
+    expect(container.firstChild).toMatchSnapshot()
+  })
+  it('should render subtitle', () => {
+    const { container } = render(<Card {...props} />)
+    expect(screen.getByText(/License type:/i)).toBeInTheDocument()
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
